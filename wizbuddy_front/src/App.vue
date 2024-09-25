93bf1468208c130e0974f7f8456ce47f99b8ed4f
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-09-25 17:54:27 +0900

    Vue set

---
+++ wizbuddy_front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppHeader />
+  <main>
+    <RouterView />
+  </main>
+  <AppFooter />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+
+import AppHeader from '@/components/AppHeader.vue';
+import AppFooter from '@/components/AppFooter.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ wizbuddy_front/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">
+                로고
+            </RouterLink>
+            
+        </nav>        
+
+    </header>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const user = ref("");  
+
+</script>
+
+<style scoped>
+
+</style>
