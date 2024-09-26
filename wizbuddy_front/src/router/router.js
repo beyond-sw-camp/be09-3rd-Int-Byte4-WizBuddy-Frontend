a3915f824d7939bdd95d69cb75a0878b1b198329
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-09-26 16:52:55 +0900

    스케줄 메인화면 추가 및 AppHeader, AppFooter 일부 수정

--- wizbuddy_front/src/router/router.js
+++ wizbuddy_front/src/router/router.js
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "@/views/MainPage.vue";
 import LoginPage from "@/views/LoginPage.vue";
+import ScheduleMainPage from "@/views/ScheduleMainPage.vue";
+// import CheckListPage from '@/views/CheckListPage.vue'; // CheckListPage 컴포넌트 경로 확인
+// import NoticeBoardPage from '@/views/NoticeBoardPage.vue'; // NoticeBoardPage 컴포넌트 경로 확인
+// import ManualBoardPage from '@/views/ManualBoardPage.vue'; // ManualBoardPage 컴포넌트 경로 확인
+// import SubsBoardPage from '@/views/SubsBoardPage.vue'; // SubsBoardPage 컴포넌트 경로 확인
 
 const routes = [
+  { path: '/schedule', component: ScheduleMainPage },
+  // { path: '/checklist', component: CheckListPage },
+  // { path: '/noticeboard', component: NoticeBoardPage },
+  // { path: '/manualboard', component: ManualBoardPage },
+  // { path: '/subsboard', component: SubsBoardPage },
   { path: "/", component: MainPage }, // 메인 경로를 루트로 설정
   { path: "/login", component: LoginPage},
 ];
