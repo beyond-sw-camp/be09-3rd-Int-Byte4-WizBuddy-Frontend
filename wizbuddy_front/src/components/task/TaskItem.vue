117067825a9352ed7da2a6cfebc1521c34378c6c
Hellin22 <dno06117@naver.com>
2024-09-27 10:19:15 +0900

    ♻️ 코드 수정: prettier extension 적용으로 인한 수정

--- wizbuddy_front/src/components/task/TaskItem.vue
+++ wizbuddy_front/src/components/task/TaskItem.vue
@@ -24,20 +24,25 @@ const getTaskClass = (isFixed) => {
 
 <style scoped>
 .task-item {
-  background-color: #ffffff; /* 배경색 흰색 */
+  background-color: #ffffff;
+  /* 배경색 흰색 */
   border-radius: 10px;
   margin-bottom: 15px;
   padding: 15px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 그림자 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* 그림자 */
   display: flex;
-  justify-content: space-between; /* 아이템을 양쪽 끝에 배치 */
-  align-items: center; /* 세로 가운데 정렬 */
+  justify-content: space-between;
+  /* 아이템을 양쪽 끝에 배치 */
+  align-items: center;
+  /* 세로 가운데 정렬 */
 }
 
 .task-info {
   display: flex;
   flex-direction: column;
-  justify-content: center; /* 세로 가운데 정렬 */
+  justify-content: center;
+  /* 세로 가운데 정렬 */
 }
 
 .task-header {
@@ -48,23 +53,27 @@ const getTaskClass = (isFixed) => {
   padding: 5px 10px;
   border-radius: 15px;
   margin-bottom: 5px;
-  text-align: center; /* 가로 가운데 정렬 */
+  text-align: center;
+  /* 가로 가운데 정렬 */
 }
 
 .fixed-task .task-header {
-  background-color: #f28b82; /* 고정된 업무의 배경색 */
+  background-color: #f28b82;
+  /* 고정된 업무의 배경색 */
   color: white;
 }
 
 .non-fixed-task .task-header {
-  background-color: #aecbfa; /* 고정되지 않은 업무의 배경색 */
+  background-color: #aecbfa;
+  /* 고정되지 않은 업무의 배경색 */
   color: white;
 }
 
 .task-body {
   font-size: 14px;
   color: #555;
-  text-align: center; /* 가로 가운데 정렬 */
+  text-align: center;
+  /* 가로 가운데 정렬 */
 }
 
 .fixed-indicator {
